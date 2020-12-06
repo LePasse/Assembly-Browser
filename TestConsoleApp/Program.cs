d11e3a4940ad785545c5974b39f16b0eb0c86b86
LePasse <43090385+LePasse@users.noreply.github.com>
2020-12-06 18:37:55 +0300

    Assembly

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using AssemblyInfoLib;
+namespace MainFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+namespace ExtensionFunc
+{
+    public static class ExtensionClass
+    {
+        static private char Char;
+
+        static int Test(bool das)
+        {
+            return 4;
+        }
+        public static int Test3(this ExtendedClass test, bool bol,int tes)
+        {
+            return 4;
+        }
+    }
+    public class ExtendedClass
+    {
+        public char Char;
+        private int Int { get; set; }
+
+        public void Test1(int a, int b)
+        {
+        }
+
+        public float Test2(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
+namespace SimpleClasses
+{
+    public class TestClass
+    {
+        public char Char;
+        private int Int { get; set; }
+
+        public float Test(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    abstract class AbstractClass
+    {
+        public char Char;
+        private int Int;
+    }
+
+    static class StaticClass
+    {
+        static public float Test(float a, float b)
+        {
+            return a - b;
+        }
+    }
+    struct TestStruct
+    {
+        public char Char;
+        private int Int { get; set; }
+
+        public float Test(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ TreeViewWPF/ViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Reflection;
+using AssemblyInfoLib;
+using System.Windows.Input;
+
+namespace TreeViewWPF
+{
+    class ViewModel: INotifyPropertyChanged
+    {
+
+        private AssemblyInformation assemblyInformation = null;
+        private IDialogService GetFileDialog;
+        public ICommand LoadAssebmly { get { return new RelayCommand(obj => GetAsm()); }}
+
+        public AssemblyInformation AssemblyInformation
+        {
+            get { return assemblyInformation; }
+            set
+            {
+                assemblyInformation = value;
+                OnPropertyChanged(nameof(AssemblyInformation));
+            }
+        }
+
+        private void GetAsm()
+        {
+            if (GetFileDialog.OpenFileDialog())
+            {
+                AssemblyBrowser assemblyBrowser = new AssemblyBrowser();
+                Assembly assembly = Assembly.LoadFrom(GetFileDialog.FilePath);
+                AssemblyInformation = assemblyBrowser.GetAssemblyInformation(assembly);
+            }
+        }
+
+        public ViewModel()
+        {
+            GetFileDialog = new DefaultDialogService();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propname)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propname));
+            }
+        }
+    }
+}
